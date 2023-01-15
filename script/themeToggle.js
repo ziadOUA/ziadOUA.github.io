531c8c015b06fee8b5146cb856e891d3baea5e68
ziadOUA <ziadouapro@outlook.com>
2023-01-15 13:45:18 +0100

    Added the favicon (See CHANGELOG.md for full details)

--- script/themeToggle.js
+++ script/themeToggle.js
@@ -1,23 +1,42 @@
+const favicon16x16 = document.querySelector('link[sizes="16x16"]')
+const favicon32x32 = document.querySelector('link[sizes="32x32"]')
+
 function lightTheme() {
-    document.getElementById("theme-icon").textContent = "dark_mode"
+    document.getElementById("theme-icon").textContent = "dark_mode";
     document.documentElement.setAttribute('theme', 'light');
 }
 
 function darkTheme() {
-    document.getElementById("theme-icon").textContent = "light_mode"
+    document.getElementById("theme-icon").textContent = "light_mode";
     document.documentElement.setAttribute('theme', 'dark');
 }
 
 (function() {
     let onPageLoad = localStorage.getItem("theme") || "";
     if (onPageLoad === "") {
-        localStorage.setItem("theme", "light_mode");
+        if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+            localStorage.setItem("theme", "light_mode");
+            lightTheme()
+        }
+        else {
+            localStorage.setItem("theme", "dark_mode");
+            darkTheme()
+        }
+    }
+
+    if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        favicon32x32.setAttribute('href', 'res/favicon/light/favicon-32x32.png')
+        favicon16x16.setAttribute('href', 'res/favicon/light/favicon-16x16.png')
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        favicon32x32.setAttribute('href', 'res/favicon/dark/favicon-32x32.png')
+        favicon16x16.setAttribute('href', 'res/favicon/dark/favicon-16x16.png')
+    }
+
+    if (onPageLoad === "light_mode") {
         lightTheme()
     }
-    else if (onPageLoad === "light_mode") {
-        lightTheme()
-    }
-    else if (onPageLoad === "dark_mode") {
+    if (onPageLoad === "dark_mode") {
         darkTheme()
     }
 })();
