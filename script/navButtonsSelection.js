b75df207b8ca75265a5b26dda6e9fdd04bcf1aa5
ziadOUA <ziadouapro@outlook.com>
2023-01-19 22:31:51 +0100

    Started preparation for the other parts of the website (See CHANGELOG.md for full details)

--- script/navButtonsSelection.js
+++ script/navButtonsSelection.js
@@ -7,57 +7,59 @@ const infoIcon = document.getElementById("info-icon");
 const contactIcon = document.getElementById("contact-icon");
 // const mobileContactIcon = document.getElementById("mobile-contact-icon");
 
-// const homeContent = document.getElementById("home-content")
-// const reposContent = document.getElementById('repos-content')
-// const infoContent = document.getElementById("info-content")
-// const contactContent = document.getElementById("contact-content")
+const homeContent = document.getElementById("home-page")
+const reposContent = document.getElementById('repos-page')
+const infoContent = document.getElementById("info-page")
+const contactContent = document.getElementById("contact-page")
 
 function homeSelectedStyling() {
     homeIcon.classList.add("selected");
     // mobileHomeIcon.classList.add("selected");
-    // homeContent.className = "home-content visible";
+    homeContent.className = "home-page visible";
+    sleep(1000).then();
+    homeContent.style.display = 'block';
 }
 
 function homeUnselectedStyling() {
     homeIcon.classList.remove("selected");
     // mobileHomeIcon.classList.remove("selected");
-    // homeContent.className = "home-content hidden";
+    homeContent.className = "home-page hidden";
 }
 
 function infoSelectedStyling() {
     infoIcon.classList.add("selected");
     // mobileInfoIcon.classList.add("selected");
-    // infoContent.className = "info-content visible";
+    infoContent.className = "info-page visible";
 }
 
 function infoUnselectedStyling() {
     infoIcon.classList.remove("selected");
     // mobileInfoIcon.classList.remove("selected");
-    // infoContent.className = "info-content hidden";
+    infoContent.className = "info-page hidden";
 }
 
 function reposSelectedStyling() {
     reposIcon.classList.add("selected");
     // mobileReposIcon.classList.add("selected");
-    // reposContent.className = "repos-content visible";
+    reposContent.className = "repos-page visible";
 }
 
 function reposUnselectedStyling() {
     reposIcon.classList.remove("selected");
     // mobileReposIcon.classList.remove("selected");
-    // reposContent.className = "repos-content hidden";
+    reposContent.className = "repos-page hidden";
 }
 
 function contactSelectedStyling() {
     contactIcon.classList.add("selected");
     // mobileContactIcon.classList.add("selected");
-    // contactContent.className = "contact-content visible";
+    contactContent.className = "contact-page visible";
 }
 
 function contactUnselectedStyling() {
     contactIcon.classList.remove("selected");
     // mobileContactIcon.classList.remove("selected");
-    // contactContent.className = "contact-content hidden";
+    contactContent.className = "contact-page hidden";
 }
 
 function homeSelected() {
